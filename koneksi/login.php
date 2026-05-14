42d586403db828a2dc794eef95c3f63644b5ca1f
asddd7 <adipatisuryadimyati@gmail.com>
2026-05-14 10:26:02 +0700

    feat: secure cookie session

--- koneksi/login.php
+++ koneksi/login.php
@@ -1,6 +1,19 @@
 <?php
+
+ini_set('session.use_only_cookies', 1);
+ini_set('session.cookie_httponly', 1);
+ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path'     => '/',
+    'domain'   => '',
+    'secure'   => isset($_SERVER['HTTPS']),
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
+
 session_start();
-include 'koneksi.php';
 
 /*
 |--------------------------------------------------------------------------
